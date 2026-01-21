987a9b4dc9bff9f71df96ff114ccd9b8f66a24b3
yaohaofei <313888178@qq.com>
2026-01-21 10:18:14 +0800

    feat: release v1.0.0.0

---
+++ AdSetQDB.framework/Headers/AdSetQDB_AdWangMaiSDK.h
@@ -0,0 +1,39 @@
+//
+//  AdSetQDB_AdWangMaiSDK.h
+//  AdSetQDB_AdWangMaiSDK
+//
+//  Created by 周泽浩 on 2023/2/17.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AdSetQDB_AdWangMaiSDK.
+FOUNDATION_EXPORT double AdWangMaiSDKVersionNumber;
+
+//! Project version string for AdSetQDB_AdWangMaiSDK.
+FOUNDATION_EXPORT const unsigned char AdWangMaiSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdSetQDB_AdWangMaiSDK/PublicHeader.h>
+
+
+#import <AdSetQDB/AdSetQDB_AdWangMaiSDK.h>
+#import <AdSetQDB/AdSetQDB_XAdWangMaiSDk.h>
+#import <AdSetQDB/AdSetQDB_XAdSplashAd.h>
+#import <AdSetQDB/AdSetQDB_XAdSplashDelegate.h>
+
+#import <AdSetQDB/AdSetQDB_XAdRewardedVideoAd.h>
+#import <AdSetQDB/AdSetQDB_XAdRewardedVideoDelegate.h>
+
+#import <AdSetQDB/AdSetQDB_XAdInterstitialAd.h>
+#import <AdSetQDB/AdSetQDB_XAdInterstitialDelegate.h>
+
+#import <AdSetQDB/AdSetQDB_XAdNativeExpressAd.h>
+#import <AdSetQDB/AdSetQDB_XAdNativeExpressDelegete.h>
+
+#import <AdSetQDB/AdSetQDB_XAdNativeAd.h>
+#import <AdSetQDB/AdSetQDB_XAdNativeAdResponse.h>
+#import <AdSetQDB/AdSetQDB_XAdNativeDelegete.h>
+
+#import <AdSetQDB/AdSetQDB_XAdWMNativeAd.h>
+#import <AdSetQDB/AdSetQDB_XAdWMNativeAdData.h>
+#import <AdSetQDB/AdSetQDB_XAdWMNativeAdDelegate.h>
